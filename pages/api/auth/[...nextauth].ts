ae35e0a2f48bb45e978c7bec9c82f921bd4d44fb
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-02-08 00:37:56 +0000

    fix next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient()
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
       GoogleProvider({
         clientId: "1058411638662-gqhtlhb5ic6gkjj2ib1b9vc9d3sd2h29.apps.googleusercontent.com",
